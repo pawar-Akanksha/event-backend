bd69a92c1c2bc69f59b357205fd23f75aee790e7
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-03-24 09:51:29 -0700

    added

--- connection/conn.js
+++ connection/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 async function getConnection() {
-    await mongoose.connect("mongodb+srv://pawarakanksha942:even@cluster0.upa73lr.mongodb.net/event?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://pawarakanksha942:event@cluster0.upa73lr.mongodb.net/event?retryWrites=true&w=majority");
 }
 module.exports = getConnection;
